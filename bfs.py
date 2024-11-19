8a007d1b2d72057e078d8269219127a5de1c1e06
Raj-Gopika <gopika.raj511@gmail.com>
2024-11-19 12:03:58 +0000

    Added GUi with btton that regenerates the path

--- bfs.py
+++ bfs.py
@@ -1,6 +1,9 @@
 #need to get start and end position of the grid
 #also need to get any position on the grid to return the number its assossiated with
 #define an agent that can move through the grid
+# use bfs to calculate path
+# show the bfs path in terminal as well as in a different color in GUI
+#if time use animate.
 
 '''
 [0,1]
@@ -8,3 +11,5 @@
 [0,-1]
 [-1,0]
 '''
+# writing bfs class insoed program class or separate?
+#can add separate py file if needed.
